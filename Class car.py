f7a382aec850f31cba2cf9dc3039e31d52275e5b
Divyaparadkar@007 <114057036+divyaparadkar@users.noreply.github.com>
2022-12-17 00:16:56 +0530

    Add files via upload

---
+++ Class car.py
@@ -0,0 +1,22 @@
+#car class
+
+class car:
+    pass
+ford = car()
+honda = car()
+audi = car()
+
+ford.speed=200
+honda.speed =240
+audi.speedd =350
+
+ford.color ='Red'
+honda.color ='blue'
+audi.color ='black'
+
+print("colour of ford is",ford.color)
+print("speed of ford is",ford.speed)
+print("speed of honda is",honda.speed)
+print("color of audi is",audi.color)
+
+
