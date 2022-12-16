116a7411c78d2c8a8900ec0c6aff9b02fcb0dedb
Divyaparadkar@007 <114057036+divyaparadkar@users.noreply.github.com>
2022-12-17 00:53:19 +0530

    Add files via upload

---
+++ birds.py
@@ -0,0 +1,28 @@
+#bird class
+
+class Bird:
+    pass
+parrot = Bird()
+duck = Bird()
+penguin = Bird()
+flamigo = Bird()
+canary = Bird()
+Kingfisher = Bird()
+
+parrot.color = 'green'
+duck.color = 'white'
+penguin.color = 'black.white'
+flamigo.color = 'pink'
+canary.color = 'yellow'
+Kingfisher.color = 'Blue.Nevyblue'
+
+parrot.age = 12
+duck.age = 4
+penguin.age = 10
+flamigo.age = 9
+canary.age = 8
+Kingfisher.age = 13
+
+print(penguin.color,penguin.age)
+print(canary.color,canary.age)
+
