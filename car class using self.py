446f8d9ea2da3a76f3c855ef762c893feba9bd61
Divyaparadkar@007 <114057036+divyaparadkar@users.noreply.github.com>
2022-12-17 14:08:16 +0530

    Add files via upload

---
+++ car class using self.py
@@ -0,0 +1,10 @@
+#car class
+
+class car:
+    def __init__(self, speed,color):
+        print(speed)
+        print(color)
+
+ford = car(200 ,'red')
+honda = car(240,'black')
+audi = car(300,'blue')
